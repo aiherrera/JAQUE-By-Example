c1cfee7d7ad8254a0ff2cdf9f3b313733ebe56c2
Alain Iglesias <alain.iherrera@gmail.com>
2021-02-19 08:18:26 -0600

    💄 updated theme & routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,22 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject, Renderer2, ViewChild } from '@angular/core';
-import { HamburgerComponent } from './components/hamburger/hamburger.component';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   private currentTheme = 'theme-light'
-  isOpen: boolean = false
-
-  @ViewChild(SidenavComponent) sidenavComp?: SidenavComponent
-  @ViewChild(HamburgerComponent) hamburgerComp?: HamburgerComponent
-
-  get isDarkMode(): boolean {
-    return this.currentTheme === 'theme-dark'
-  }
-
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
-    private renderer: Renderer2
+    private renderer: Renderer2,
   ) {}
 
   ngOnInit(): void {
     this.currentTheme = localStorage.getItem('activeTheme') || 'theme-light'
     this.renderer.setAttribute(this.document.body, 'class', this.currentTheme)
-  }
-
-  /**
-   * Switches the app between light/dark mode
-   * @param isDarkMode sets light/dark mode
-   */
-  switchMode(isDarkMode: boolean): void {
-    this.currentTheme = isDarkMode ? 'theme-dark' : 'theme-light'
-    this.renderer.setAttribute(this.document.body, 'class', this.currentTheme)
-    localStorage.setItem('activeTheme', this.currentTheme)
-  }
-
-  /**
-   *  Toggles menu icon open/close class
-   */
-  toggle(): void {
-    this.isOpen = !this.isOpen
-    this.hamburgerComp!.onToggle(this.isOpen)
-    this.sidenavComp?.sidenav?.toggle()
   }
 }
--- src/shared/routing.module.ts
+++ src/shared/routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
+import { LoginComponent } from 'src/app/components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/log-in', pathMatch: 'full' },
+  { path: 'log-in', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
