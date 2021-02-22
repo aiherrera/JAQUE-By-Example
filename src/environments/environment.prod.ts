3316cbb775ad3c6f68e398c361e1ee88d6bf4bda
Alain Iglesias <alain.iherrera@gmail.com>
2021-02-22 11:40:26 -0600

    🔧 configured environments wtih firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDOJLsHu4QR-de4fvpag9B1IoGLeDnGg6A",
+    authDomain: "jaqueeval-3c3c6.firebaseapp.com",
+    projectId: "jaqueeval-3c3c6",
+    storageBucket: "jaqueeval-3c3c6.appspot.com",
+    messagingSenderId: "634971055286",
+    appId: "1:634971055286:web:040a4d4fb466d6bfcbb217",
+    measurementId: "G-LW6NDB4Q3P"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDOJLsHu4QR-de4fvpag9B1IoGLeDnGg6A",
+    authDomain: "jaqueeval-3c3c6.firebaseapp.com",
+    projectId: "jaqueeval-3c3c6",
+    storageBucket: "jaqueeval-3c3c6.appspot.com",
+    messagingSenderId: "634971055286",
+    appId: "1:634971055286:web:040a4d4fb466d6bfcbb217",
+    measurementId: "G-LW6NDB4Q3P"
+  }
 };
 
 /*
